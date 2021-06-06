38a349b17869ca224ff2b3ec3273a648a201d219
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-06-05 21:10:32 -0300

    Create hello-world.c

---
+++ hello-world.c
@@ -0,0 +1,8 @@
+/* Primeiro programa em C*/
+
+#include <stdio.h> /*  esta instrução insere o conteúdo do arquivo stdio.h */
+int main() /* todo programa em C deve conter essa linha */
+{ /* delimita o início das instruções */
+  printf("Hello, World!"); /* esta instrução imprime uma mensagem na tela */
+  return(0); /* este retorno indica que o programa está finalizando sem erros */
+} /* delimita o fim do conjunto de instruções da função main */
