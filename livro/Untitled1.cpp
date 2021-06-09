1b588708e25696f7c40e9ab29d4ccb72c9c90ca6
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-06-08 22:44:10 -0300

    Create Untitled1.cpp

---
+++ livro/Untitled1.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+	printf("Estou aprendendo a programar em C. ");
+	printf("Estou lendo a %d unidade do livro. ", 1);
+	printf("%s e uma disciplina importante do curso. ", "Esta");
+	printf("%f", 57.35);
+	return(0);
+}
