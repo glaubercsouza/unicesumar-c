6f72f20fd7c97b48fe9eef46707847473c6d9bd2
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-06-14 21:44:26 -0300

    Create estrutura-condicional-simples.c

---
+++ livro/estrutura-condicional-simples.c
@@ -0,0 +1,21 @@
+/* Estrutura condicional simples em C */
+/* incluindo o conteúdo de stdio.h */
+#include <stdio.h>
+/* insere o conteúdo do arquivo math, que tem a função sqrt, cálculo da raiz quadrada */
+#include <math.h>
+
+/* todo programa em C deve possuir essa linha */
+int main()
+{ /* delimita o início das instruções */
+	int 	num;
+	float 	raiz;
+	
+	printf("\nDigite um numero inteiro: ");
+	scanf("%d", &num);
+	if (num % 2 == 0)
+	{
+		raiz = sqrt(num);
+		printf("\nA raiz quadrada e: %.3f", raiz);
+	}
+	return(0); /* retorno sem erro */
+} /* fim do conjunto de instruções da função main */
