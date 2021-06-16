1972bb045a5d76cb0b7f6c66522e51297709095f
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-06-15 21:47:12 -0300

    Create problema3.c

---
+++ exercicios/unidade2/problema3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int	idade;
+	printf("Digite sua idade: ");
+	scanf("%d", &idade);
+	if (idade < 16)
+	{
+		printf("Voto não obrigatorio!");
+	} 
+	else 
+	{
+		if ((idade < 18) || (idade > 65))
+		{
+			printf("Voto facultativo!");
+		}
+		else
+		{
+			printf("Voto obrigatorio!");
+		}
+	}
+	return(0);
+}
