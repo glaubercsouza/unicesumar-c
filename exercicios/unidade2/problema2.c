f33dc361b36a1bddc0464255a1ea8c149447c784
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-06-15 21:50:04 -0300

    Update problema2.c

--- exercicios/unidade2/problema2.c
+++ exercicios/unidade2/problema2.c
@@ -20,23 +20,25 @@ int main()
 		if ((idade > 18) && (idade <= 29))
 		{
 			printf("Valor da mensalidade: R$ 70,00\n");
-			}	
+		}	
+		else
+		{
+			if ((idade > 29) && (idade <= 45))
+			{
+				printf("Valor da mensalidade: R$ 90,00\n");
+			}
 			else
 			{
-				if ((idade > 29) && (idade <= 45))
+				if (( idade > 45) && (idade <= 65))
 				{
-					printf("Valor da mensalidade: R$ 90,00\n");
+					printf("Valor da mensalidade: R$ 130,00\n");
 				}
-				else{
-					if (( idade > 45) && (idade <= 65))
-					{
-						printf("Valor da mensalidade: R$ 130,00\n");
-					}
-					else{
-						printf("Valor da mensalidade: R$ 170,00\n");
-					}
+				else
+				{
+					printf("Valor da mensalidade: R$ 170,00\n");
 				}
 			}
+		}
 	}
 	return(0);
 }
