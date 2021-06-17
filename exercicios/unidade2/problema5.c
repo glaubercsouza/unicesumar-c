891700a991d0035b0ad1a0b86c53ce74707ea6be
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-06-16 21:31:55 -0300

    Create problema5.c

---
+++ exercicios/unidade2/problema5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+	int		opcao;
+	float 	calorias;
+	printf("Escolha um prato tipico:\n1 - Italiano\n2 - Japones\n3 - Salvadorenho\n");
+	scanf("%d", &opcao);
+	switch (opcao)
+	{
+		case 1:
+			calorias = calorias + 750;
+			break;
+		case 2:
+			calorias = calorias + 324;
+			break;
+		case 3:
+			calorias = calorias + 545;
+			break;
+	}
+	printf("Escolha uma bebida:\n1 - Cha \n2 - Suco de laranja \n3 - Refrigerante\n");
+	scanf("%d", &opcao);
+	switch (opcao)
+	{
+		case 1:
+			calorias = calorias + 30;
+			break;
+		case 2:
+			calorias = calorias + 80;
+			break;
+		case 3:
+			calorias = calorias + 90;
+			break;
+	}
+	printf("Quantidade de calorias ingeridas: %.0f", calorias);
+	return(0);
+}
