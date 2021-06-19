1b0cc5cc433a30d39db61299f365d6b2bd342824
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-06-19 19:48:10 -0300

    Create atividade1.c

---
+++ atividade1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	float	salarioAtual, novoSalario;
+	printf("***************************************************\n* EMPRESA ATIVIDADE-01 :)\n* Nome: Glauber Costa de Souza\n* RA: 20143994-5\n*\n***************************************************\n* Qual foi o seu último salário líquido recebido?\n* R$ ");
+	scanf("%f", &salarioAtual);
+	novoSalario = salarioAtual * 1.075;
+	if(novoSalario <= 1750){
+		novoSalario += 150;
+	}
+	printf("*\n***************************************************\n* O último salário recebido foi de R$ %.2f.\n* O seu próximo salário será de R$ %.2f.\n*\n***************************************************", salarioAtual, novoSalario);
+	return(0);	
+}
