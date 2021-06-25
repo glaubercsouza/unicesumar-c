2d476db67ee6651f7d2eb2b863bf94ebb66d0a29
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-06-25 20:36:32 -0300

    Update atividade1.c

--- atividade1.c
+++ atividade1.c
@@ -7,9 +7,9 @@ int main(){
 	printf("***************************************************\n* EMPRESA ATIVIDADE-01 :)\n* Nome: Glauber Costa de Souza\n* RA: 20143994-5\n*\n***************************************************\n* Qual foi o seu último salário líquido recebido?\n* R$ ");
 	scanf("%f", &salarioAtual);
 	novoSalario = salarioAtual * 1.075;
-	if(novoSalario <= 1750){
+	if(salarioAtual <= 1750){
 		novoSalario += 150;
 	}
-	printf("*\n***************************************************\n* O último salário recebido foi de R$ %.2f.\n* O seu próximo salário será de R$ %.2f.\n*\n***************************************************", salarioAtual, novoSalario);
+	printf("*\n***************************************************\n* O último salário recebido foi de R$ %.2f.\n* O seu próximo provável salário será de R$ %.2f.\n*\n***************************************************", salarioAtual, novoSalario);
 	return(0);	
 }
