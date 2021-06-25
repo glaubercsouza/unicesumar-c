930e8cabd8f13d2c15fc287d8f5e176e2740cfa8
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-06-25 20:44:04 -0300

    Update exercicio4.c

--- exercicios/unidade3/exercicio4.c
+++ exercicios/unidade3/exercicio4.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	int		i, soma;
+	for(i = 200; i <= 500; i++){
+		if(i % 2 != 0){
+			soma += i;
+		}
+	}
+	printf("\nA soma de todos os número ímpares entre 200 e 500 é %d", soma);
+}
