abab53bb8fb8a8f2d4c183d762e645b35e92048e
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-07-04 15:24:20 -0300

    Create funcoes-void.c

---
+++ livro/funcoes-void.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void soma(){
+	float	num1, num2, total;
+	printf("Digite o primeiro número: ");
+	scanf("%f", &num1);
+	printf("Digite o segundo número: ");
+	scanf("%f", &num2);
+	total = num1 + num2;
+	printf("A soma é %.2f\n", total);
+}
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	system("cls");
+	soma();
+	return(0);
+}
