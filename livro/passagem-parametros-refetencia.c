43826a082279bca861d2de3b3d02ccd4f40e1839
Glauber Souza <73006247+glaubercsouza@users.noreply.github.com>
2021-07-05 21:43:46 -0300

    Create passagem-parametros-refetencia.c

---
+++ livro/passagem-parametros-refetencia.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <locale.h>
+
+int calcQuadrado(int *x){
+	*x = *x * (*x);
+	return *x;
+}
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	int	num, resposta;
+	printf("Digite um número inteiro: ");
+	scanf("%d", &num);
+	resposta = calcQuadrado(&num);
+	printf("O quadrado do número %d é %d\n", num, resposta);
+}
